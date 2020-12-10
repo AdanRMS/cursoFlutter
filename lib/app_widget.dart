f7014beabebf215e64a0ef8c938e615d75bd6f4c
AdanRMS <adanraphael07@hotmail.com>
2020-12-10 16:21:20 -0300

    Finalizando aula #12 - Gerenciar outro Estado

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/app_controller.dart';
 
 import 'home_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: homePage(),
+    return AnimatedBuilder(
+      animation: AppController.instance,
+      builder: (context, child) {
+        return MaterialApp(
+          theme: ThemeData(
+              primarySwatch: Colors.red,
+              brightness: AppController.instance.isDarkTheme
+                  ? Brightness.dark
+                  : Brightness.light),
+          home: homePage(),
+        );
+      },
     );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app_controller.dart';
+
 class homePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -8,18 +10,17 @@ class homePage extends StatefulWidget {
 }
 
 class homePageState extends State<homePage> {
-  var cont = 0;
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: GestureDetector(
-          child: Text('Contador $cont'),
-          onTap: () {
-            setState(() {
-              cont++;
-            });
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contador Monstro'),
+      ),
+      body: Center(
+        child: Switch(
+          value: AppController.instance.isDarkTheme,
+          onChanged: (value) {
+            AppController.instance.changeTheme();
           },
         ),
       ),
