79a9415fd07e81a06126f8047b192586671528f0
AdanRMS <adanraphael07@hotmail.com>
2020-12-10 14:57:16 -0300

    Finalizando aula #11 - Single render e Multi render

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,20 +8,34 @@ class homePage extends StatefulWidget {
 }
 
 class homePageState extends State<homePage> {
-  var cont = 0;
+  int cont = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: GestureDetector(
-          child: Text('Contador $cont'),
-          onTap: () {
-            setState(() {
-              cont++;
-            });
-          },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contador Monstro'),
+      ),
+      body: Container(
+        height: 200,
+        width: 200,
+        color: Colors.black,
+        child: Center(
+          //center provê um novo render, assims sendo possível desenhar 2 quadrados na tela
+          child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            cont++;
+          });
+        },
       ),
     );
   }
